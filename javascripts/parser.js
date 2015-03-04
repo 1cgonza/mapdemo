11e16f6cd711e72db0db653e268d5ae46faaba96
Juan Camilo Gonzalez <info@juancgonzalez.com>
2015-03-04 14:07:29 +0800

    simpler version

--- javascripts/parser.js
+++ javascripts/parser.js
@@ -4,80 +4,45 @@ $(function () {
     url: './data/' + file,
     dataType: 'xml'
   }).done( function (rawData) {
-    var minlat = $(rawData).find('metadata').find('bounds').attr('minlat');
-    var maxlat = $(rawData).find('metadata').find('bounds').attr('maxlat');
-    var minlon = $(rawData).find('metadata').find('bounds').attr('minlon');
-    var maxlon = $(rawData).find('metadata').find('bounds').attr('maxlon');
     var wpts   = $(rawData).find('wpt');
     var trks   = $(rawData).find('trk');
-    var tracksCounter = 0;
-
-    var parsedData = {
-      metadata: { minlat: minlat, maxlat: maxlat, minlon: minlon, maxlon: maxlon },
-      tracks: {}
-    };
+    var points = [];
 
     if (wpts && wpts.length > 0) {
-
       $(wpts).each(function () {
-        var name = $(this).find('name').html();
-
-        parsedData.tracks[tracksCounter] = { name: name, points: {} };
-        extractPointData( $(this) );
-        tracksCounter++;
+        extractPoints( $(this) );
       });
-
     }
 
     if (trks && trks.length > 0) {
       $(trks).each(function (key, value) {
-        var name = $(this).find('name').html();
-
-        parsedData.tracks[tracksCounter] = { name: name, points: {} };
-        extractPointData( $(this).find('trkpt') );
-        tracksCounter++;
+        extractPoints( $(this).find('trkpt') );
       });
     }
 
-    // console.save(parsedData, 'data.json');
-    // console.log(parsedData);
+    printDataOnScreen(points);
 
+    function extractPoints (data) {
+      $(data).each(function () {
+        var lat = $(this).attr('lat');
+        var lon = $(this).attr('lon');
 
-    function extractPointData (data) {
-      $(data).each(function (key, value) {
-        var lat       = value.getAttribute('lat')  ? value.getAttribute('lat') : null;
-        var lon       = value.getAttribute('lon')  ? value.getAttribute('lon') : null;
-        var elevation = value.getAttribute('ele')  ? value.getAttribute('ele').innerHTML : null;
-        var time      = value.getAttribute('time') ? value.getAttribute('time').innerHTML : null;
-
-        parsedData.tracks[tracksCounter].points[key] = { lat: lat, lon: lon, elevation: elevation, time: time };
+        points.push([lat, lon]);
       });
     }
 
+    function printDataOnScreen (array) {
+      $('body').append('var points = [');
+
+      for (var i = 0; i < array.length; i++) {
+        $('body').append('[' + array[i][0] + ', ' + array[i][1] + ']');
+
+        if (i !== array.length - 1) {
+          $('body').append(',');
+        }
+      }
+
+      $('body').append('];');
+    }
   });
 });
-
-(function(console){
-  console.save = function(data, filename) {
-    if(!data) {
-      console.error('Console.save: No data');
-      return;
-    }
-
-    if(!filename) filename = 'console.json';
-
-    if(typeof data === "object"){
-      data = JSON.stringify(data, undefined, 4);
-    }
-
-    var blob = new Blob([data], {type: 'text/json'}),
-        e    = document.createEvent('MouseEvents'),
-        a    = document.createElement('a');
-
-    a.download = filename;
-    a.href = window.URL.createObjectURL(blob);
-    a.dataset.downloadurl =  ['text/json', a.download, a.href].join(':');
-    e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-    a.dispatchEvent(e);
-  };
-})(console);
--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,48 +1,28 @@
-$(function () {
+document.addEventListener('DOMContentLoaded', function () {
   var mapContainer = document.getElementById('map');
-  L.mapbox.accessToken = 'pk.eyJ1IjoianVhbmNnb256YSIsImEiOiJwTzBYblFBIn0.mrChbL1APmRc2iRak665kQ';
   mapContainer.style.height = window.innerHeight + 'px';
-  var points = [];
-  var minlat, maxlat, minlon, maxlon;
 
-  $.getJSON('./data/vish.json', function (data) {
+  L.mapbox.accessToken = 'pk.eyJ1IjoianVhbmNnb256YSIsImEiOiJwTzBYblFBIn0.mrChbL1APmRc2iRak665kQ';
 
-    getAllPoints(data);
-  }).done(function () {
-    drawMap();
-  }).fail(function (error) {
-    $('body').append(error.responseText);
-  });
-
-  function drawMap () {
-    var canvasTiles = L.tileLayer.canvas();
-
-    var map = L.mapbox.map('map', 'juancgonza.89abf372').setView([19.1562, 72.7631], 11);
-
-    L.canvasOverlay().drawing(drawPoints).addTo(map);
-  }
-
-  function getAllPoints (data) {
-    for ( var i = 0; i < Object.keys(data.tracks).length; i++ ) {
-      for ( var j = 0; j < Object.keys(data.tracks[i].points).length; j++ ) {
-        var lat = data.tracks[i].points[j].lat;
-        var lon = data.tracks[i].points[j].lon;
-
-        points.push([lat, lon]);
-      }
-    }
-  }
-
-  function drawPoints (canvasOverlay, params) {
-    var ctx = params.canvas.getContext('2d');
-    ctx.clearRect(0, 0, params.canvas.width, params.canvas.height);
-    ctx.fillStyle = 'rgba(255, 116, 0, 0.2)';
-    ctx.beginPath();
-    points.map(function (d, i) {
-      dot = canvasOverlay._map.latLngToContainerPoint( [d[0], d[1]] );
-      ctx.lineTo(dot.x, dot.y);
-    });
-    ctx.stroke();
-  }
-
+  drawMap();
 });
+
+
+function drawMap () {
+  var canvasTiles = L.tileLayer.canvas();
+  var map = L.mapbox.map('map', 'juancgonza.89abf372').setView([19.1562, 72.7631], 11);
+
+  L.canvasOverlay().drawing(drawPoints).addTo(map);
+}
+
+function drawPoints (canvasOverlay, params) {
+  var ctx = params.canvas.getContext('2d');
+  ctx.clearRect(0, 0, params.canvas.width, params.canvas.height);
+  ctx.fillStyle = 'rgba(255, 116, 0, 0.2)';
+  ctx.beginPath();
+  points.map(function (d, i) {
+    dot = canvasOverlay._map.latLngToContainerPoint( [d[0], d[1]] );
+    ctx.lineTo(dot.x, dot.y);
+  });
+  ctx.stroke();
+}
